a80f0a8c2c95ac046136f40b7a5c775286f82e95
Andy <zhumeilun0103@gmail.com>
2024-10-03 22:14:50 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection.Emit;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework5_Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+
+        static Random generator = new Random();
+        static int num = generator.Next(101);
+        static int numGuess = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double guess = Convert.ToDouble(text.Text);
+
+            if (guess < num)
+            {
+                result.Content = "Guess too low!";
+                numGuess++;
+            }
+            else if (guess > num)
+            {
+                result.Content = "Guess too high!";
+                numGuess++;
+            }
+            else {
+                numGuess++;
+                result.Content = "Correct!\n"+numGuess+" guesses taken!";
+            }
+        }
+    }
+}
